2f02093bdd6b69074ef3b84c847216550d8fd09a
Michael Valentino <michael.valentino92@gmail.com>
2019-02-14 15:08:11 -0500

    Add files via upload

--- rref.hs
+++ rref.hs
@@ -2,9 +2,11 @@ rowKill :: (Fractional a) => [a] -> [a] -> [a]
 rowKill (x:xs) (y:ys) = zipWith (\x' y' -> x'*k + y') (x:xs) (y:ys)
                where k = -y/x
 
-ref :: (Fractional a) => [[a]] -> [[a]]
+ref :: (Fractional a,Eq a) => [[a]] -> [[a]]
 ref [xs] = let x = head xs in [map (/x) xs]
-ref (xs:xss) = xs' : (ref $ map tail xss')
+ref (xs:xss)
+           | x /= 0 = xs' : (ref $ map tail xss')
+           | otherwise = ref $ swapRows xs xss
             where xss' = map (rowKill xs) xss
                   xs' = map (/x) xs
                   x = head xs
@@ -19,5 +21,10 @@ bref (xs:xss) = xs : (bref xss'')
 trimDiag :: (Num a) => [[a]] -> [[a]]
 trimDiag xss = [(drop i xs) | (i,xs) <- zip [1..] xss]
 
-rref :: (Fractional a) => [[a]] -> [a]
+swapRows :: (Num a,Eq a) => [a] -> [[a]] -> [[a]]
+swapRows xs (xs':xss) 
+          | head xs == 0 = (swapRows xs' xss) ++ [xs]
+          | otherwise = [xs] ++ (xs':xss)
+
+rref :: (Fractional a,Eq a) => [[a]] -> [a]
 rref = map last . reverse . bref . reverse . ref
