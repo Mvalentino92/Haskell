b97d29c3f8d05e402c81e91bda14879772394c56
Michael Valentino <michael.valentino92@gmail.com>
2019-02-26 10:41:58 -0500

    Update determinant.hs

--- determinant.hs
+++ determinant.hs
@@ -12,6 +12,6 @@ det mat
 det' :: [[Double]] -> Double
 det' mat 
     | length mat == 2 = foldr1 (-) $ zipWith (*) (head mat) (reverse $ last mat)
-    | otherwise = sum $ zipWith (*) coefs $ map det mat'
+    | otherwise = sum $ zipWith (*) coefs $ map det' mat'
     where coefs = zipWith (*) (cycle [1,-1]) (head mat)
           mat' = [map (\x -> take i x ++ drop (i+1) x) (tail mat) | i <- [0..length mat - 1]]
